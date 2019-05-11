eee2005b6140478009781e59dec2430aa9390fcf
root <root@localhost.localdomain>
2019-05-11 20:20:22 +0100

    installer.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+################
+if hash composer; then
+  echo "Composer Already Installed"
+  echo "You can find it at $(type -p composer)"
+  composer install
+  exit
+fi
+
+# Update package index
+sudo apt update
+
+# Make sure php is installed
+sudo apt install php php-cli
+
+# Download latest composer snapshot and run it by php
+sudo wget https://getcomposer.org/composer.phar
+
+# Move composer to /bin/composer
+sudo mv composer.phar /bin/composer
+
+echo "Composer Installed Globally Successfully"
+echo "Composer Installed at $(type -p composer)"
+echo "You can always find it by calling `type -p composer`"
+php composer install
